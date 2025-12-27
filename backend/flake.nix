08b04a1f96f4f928cf90df6a9853de8e4e10cdab
Martin Larsson <larssonmartin1998@gmail.com>
2025-12-27 12:04:51 +0100

    Add flake for backend

---
+++ backend/flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Urverk backend";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        CGO = "0";
+        go = pkgs.go;
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "urverk-backend";
+          version = "0.1.0";
+          src = ./.;
+
+          vendorHash = "sha256-coScfB856+y9Gb3j8MmPXHc6ETM28+qaI2WoPA0moSA=";
+
+          env = {
+            CGO_ENABLED = CGO;
+          };
+
+          doCheck = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            delve
+            gotools
+          ];
+
+          env = {
+            CGO_ENABLED = CGO;
+          };
+
+          shellHook = ''
+            echo "🦫 $(${go}/bin/go version) ready!"
+            echo "CGO_ENABLED: $CGO_ENABLED"
+          '';
+        };
+      }
+    );
+}
